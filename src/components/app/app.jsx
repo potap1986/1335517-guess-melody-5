d1fee98ecaa9924df1c6921abadb4ec807316427
potap1986 <potap1986@inbox.ru>
2020-10-09 16:31:52 +0300

    Учебный проект: обратная сторона реальности

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,66 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen";
+import AuthScreen from "../auth-screen/auth-screen";
+import GameOverScreen from "../game-over-screen/game-over-screen";
+import WinScreen from "../win-screen/win-screen";
+import GameScreen from "../game-screen/game-screen";
 
 
 const App = (props) => {
-  const {errorsCount} = props;
+  const {errorsCount, questions} = props;
+  const [firstQuestion, secondQuestion] = questions;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact
+          path="/"
+          render={({history}) => (
+            <WelcomeScreen
+              onPlayButtonClick={() => history.push(`/game`)}
+              errorsCount={errorsCount}
+            />
+          )}
+        />
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen
+            question={secondQuestion}
+            onAnswer={() => {}}
+          />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen
+            question={firstQuestion}
+            onAnswer={() => {}}
+          />
+        </Route>
+        <Route exact path="/login">
+          <AuthScreen />
+        </Route>
+        <Route exact path="/result">
+          <WinScreen />
+        </Route>
+        <Route exact path="/lose">
+          <GameOverScreen />
+        </Route>
+        <Route exact path="/game">
+          <GameScreen
+            errorsCount={errorsCount}
+            questions={questions}
+          />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  questions: PropTypes.array.isRequired,
 };
 
 export default App;
