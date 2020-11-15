3877552f4030e4f7cc21c95b8e0e9e39c53bca7d
potap1986 <potap1986@inbox.ru>
2020-11-15 18:13:43 +0300

    Учебный проект: истина где-то на сервере

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,45 +1,55 @@
 import React from "react";
-import {Switch, Route, BrowserRouter} from "react-router-dom";
+import {Switch, Route, Router as BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
 import AuthScreen from "../auth-screen/auth-screen";
 import GameOverScreen from "../game-over-screen/game-over-screen";
 import WinScreen from "../win-screen/win-screen";
 import GameScreen from "../game-screen/game-screen";
-import {MAX_MISTAKE_COUNT} from "../../const";
+import PrivateRoute from "../private-route/private-route";
+import browserHistory from "../../browser-history";
+import {MAX_MISTAKE_COUNT, AppRoute} from "../../const";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter history={browserHistory}>
       <Switch>
         <Route exact
-          path="/"
+          path={AppRoute.ROOT}
           render={({history}) => (
             <WelcomeScreen
-              onPlayButtonClick={() => history.push(`/game`)}
+              onPlayButtonClick={() => history.push(AppRoute.GAME)}
               errorsCount={MAX_MISTAKE_COUNT}
             />
           )}
         />
-        <Route exact path="/login">
-          <AuthScreen />
-        </Route>
         <Route exact
-          path="/result"
+          path={AppRoute.LOGIN}
           render={({history}) => (
-            <WinScreen
-              onReplayButtonClick={() => history.push(`/game`)}
+            <AuthScreen
+              onReplayButtonClick={() => history.push(AppRoute.GAME)}
             />
           )}
         />
+        <PrivateRoute
+          exact
+          path={AppRoute.RESULT}
+          render={({history}) => {
+            return (
+              <WinScreen
+                onReplayButtonClick={() => history.push(AppRoute.GAME)}
+              />
+            );
+          }}
+        />
         <Route exact
-          path="/lose"
+          path={AppRoute.LOSE}
           render={({history}) => (
             <GameOverScreen
-              onReplayButtonClick={() => history.push(`/game`)}
+              onReplayButtonClick={() => history.push(AppRoute.GAME)}
             />
           )}
         />
-        <Route exact path="/game">
+        <Route exact path={AppRoute.GAME}>
           <GameScreen
             errorsCount={MAX_MISTAKE_COUNT}
           />
--- src/const.js
+++ src/const.js
@@ -3,4 +3,22 @@ export const GameType = {
   GENRE: `genre`,
 };
 
+export const AuthorizationStatus = {
+  AUTH: `AUTH`,
+  NO_AUTH: `NO_AUTH`,
+};
+
 export const MAX_MISTAKE_COUNT = 3;
+
+export const AppRoute = {
+  LOGIN: `/login`,
+  LOSE: `/lose`,
+  RESULT: `/result`,
+  ROOT: `/`,
+  GAME: `/game`,
+};
+
+export const APIRoute = {
+  QUESTIONS: `/questions`,
+  LOGIN: `/login`,
+};
